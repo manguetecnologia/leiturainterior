385edf4c25d96eca46f5a75d628ac90e877d3897
manguetecnologia <testemangue@gmail.com>
2017-08-03 21:08:01 -0300

    Seção de Contato quase finalizada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -379,7 +379,7 @@
 	                            						</div>
 	                            						<div class="col-md-6">
 	                            							<style type="text/css">iframe{ margin-left: 200px; }</style>
-	                            							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31597.684278653654!2d-34.91551263114035!3d-8.130923925396246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f0bc42e306d%3A0x2bf385bd7cec6239!2sPraia+de+Boa+Viagem!5e0!3m2!1spt-BR!2sbr!4v1501686981872" width="621" height="490" frameborder="0" style="border:0" allowfullscreen></iframe>
+	                            							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31597.684278653654!2d-34.91551263114035!3d-8.130923925396246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1f0bc42e306d%3A0x2bf385bd7cec6239!2sPraia+de+Boa+Viagem!5e0!3m2!1spt-BR!2sbr!4v1501686981872" width="621" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 	                            						</div>
 
